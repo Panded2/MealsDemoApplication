7fc448cc652f002ed3c8aa75a84db19df0bd563d
shushrut <shushrutdeshpande@shushruts-MacBook-Pro.local>
2021-10-13 11:58:55 +0530

    git commit the changes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hey lets start Restorant application coding!! </Text>
+      <Text> Hey lets start Restorant application coding!! we can try now commiting </Text>
     </View>
   );
 }
